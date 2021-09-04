ab37b70a0ec61155e276444ea2975ae05c0e4713
ali <alihaghparast@live.com>
2021-09-04 10:58:22 +0430

    requests in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'Issue tracker': f'https://github.com/{owner}/{name}/issues',
     },
     packages=find_packages(),
-    install_requires=['django', 'celery'],
+    install_requires=['django', 'celery','requests'],
     python_requires='>=3.6',
     zip_safe=False,
     classifiers=[
