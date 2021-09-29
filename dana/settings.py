223fee1a007a64e82757a780439f39863755eee9
ali <alihaghparast@live.com>
2021-09-29 16:21:05 +0330

    reade setting from moz

--- dana/settings.py
+++ dana/settings.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 
-USERNAME = 'imshoeibi@gmail.com'
-PASSWORD = 'Melk@1390'
-SECRET = '168927E1005A3046AC081020A831304184DCFD9F'
+USERNAME = settings.DANA['USERNAME']
+PASSWORD = settings.DANA['PASSWORD']
+SECRET = settings.DANA['SECRET']
